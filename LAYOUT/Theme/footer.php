cb3c6649f2e3f37a64303348158721d0d7884f04
Alex Santos <alexsantosinformatica@gmail.com>
2018-04-25 21:31:03 -0300

    Alteração Banco

---
+++ LAYOUT/Theme/footer.php
@@ -0,0 +1,12 @@
+      <!--main content end-->
+      <!--footer start-->
+      <footer class="site-footer">
+          <div class="text-center">
+              2014 - Alvarez.is
+              <a href="basic_table.html#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
+      <!--footer end-->
+  </section>
