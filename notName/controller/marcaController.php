1d05e8807e63fdf1e86f13d2d146ad0ef84d89b9
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2018-05-15 15:23:53 -0300

    teste dal

--- notName/controller/marcaController.php
+++ notName/controller/marcaController.php
@@ -3,6 +3,12 @@ require_once '../dal/MarcaDAL.php';
 
 $putinhaloira = MarcaDAL::buscaMarca();
 
-echo $putinhaloira;
+foreach ($putinhaloira as $p) {
+    // echo "Codigo da Marca: " + $p->getIdMarca()+"<br/>";
+    // echo "Nome da Marca: "+ $p->getDescMarca()+"<br/>";
+    echo "Codigo da Marca: " . $p->getIdMarca() . "<br />";
+    echo "Nome da Marca: " . $p->getDescMarca() . "<br />";
+    echo "Status da Marca: " . $p->getStatusMarca() . "<br />";
+}
 
 ?>
