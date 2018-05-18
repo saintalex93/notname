5b0f873020bf87e3be2b3cac24148ae331ba8d02
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2018-05-18 17:42:26 -0300

    Cadastro

--- notName/index.php
+++ notName/index.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Teste</title>
-    </head>
-    <body>
+<head>
+<title>Teste</title>
+</head>
+<body>
 
-        <form action='./controller/inseremarcaController.php' method='GET'>
-            <label>Nome: </label><input type='text'  name='name'> <br><br>
-            <label>Status: </label><input type='number' name='statusMar'> <br><br>
-            <button type='submit'>Insere</button>
-        </form>
+	<form action='./controller/inseremarcaController.php' method='GET'>
+		<label>Nome: </label><input type='text' name='name'> <br>
+		<br> <label>Status: </label><input type='number' name='statusMar'> <br>
+		<br>
+		<button type='submit'>Insere</button>
+	</form>
 
-        <form action='./controller/marcaController.php' method='POST'>
-            <button>Buscar</button>
-        </form>
-    </body>
+	<form action='./controller/marcaController.php' method='POST'>
+		<button>Buscar</button>
+	</form>
+<br>
+	<form action='./controller/ClienteController.php' method='POST'>
+		<input type="text" placeholder="Nome" name="nome" id="nome"> <br><br>
+		<input type="email" placeholder="Email" name="email" id="email"> <br><br>
+		<input type="password" placeholder="Senha" name="senha" id="senha"> <br><br>
+		<button>Enviar</button>
+		
+	</form>
+</body>
 </html>
