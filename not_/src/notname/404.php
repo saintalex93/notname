f3a045f6b56fc65b99ea602e76df25b8cf9b422e
Diego <diego.rodrigues.joaquim@gmail.com>
2018-05-27 12:36:18 -0300

    Nova pasta (not_)

---
+++ not_/src/notname/404.php
@@ -0,0 +1,45 @@
+<?php include "superior.php";?>
+
+          <div id="content">
+            <div class="container">
+
+                <div class="col-md-12">
+
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+
+                            <li class="breadcrumb-item active" aria-current="page">página não encontrada</li>
+                        </ol>
+                    </nav>
+
+
+
+                    <div class="row" >
+                        <div class="col-md-6 offset-md-3">
+                            <div class="card text-center">
+
+                                <p class="text-center">
+                                    <img src="assets/logo.png" alt="">
+                                </p>
+
+                                <h3>Lamentamos - esta página não está mais aqui</h3>
+                                <h4 class="text-muted">Erro 404 - Página Não Encontrada</h4>
+
+                                <p class="text-center">Para continuar, use o <strong>formulário de pesquisa</strong> ou o   <strong>Menu</strong> acima</p>
+
+                                <p class="buttons"><a href="index.php" class="btn btn-primary"><i class="fa fa-home"></i> Vá para a página inicial</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+   
+            </div>
+   
+        </div>
+
+    <?php include_once "inferior.php";?>
+
