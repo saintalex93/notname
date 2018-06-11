4e46df5a3bb9b7c0ab3ea4d7589e745a7066c3ec
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2018-06-11 11:58:43 -0300

    Alteração do banco

--- notName/painel/js/CadastraUsrSys.js
+++ notName/painel/js/CadastraUsrSys.js
@@ -3,10 +3,13 @@ $("#btnCadastroUsr").click(function(event){
 	if ($("#txtNomeUsr").val() != '' 
 		&& $("#txtEmail").val() != '' 
 		&& $("#txtLogin").val() != '' 
-		&& $("#txtSenhaUsr").val() != '' 
+		&& $("#txtSenhaUsr").val() != ''
 		&& $("#permissao").val() != '' 
 		&& $("#statusUsuario").val() != '') 
 	{
+
+		$("#returnCadastroUsr").text("");
+		$("#returnCadastroUsr").css("display","block");
 
 		var data = $("#formUsrSys").serialize();
 
@@ -20,14 +23,33 @@ $("#btnCadastroUsr").click(function(event){
 				var resposta = JSON.parse(response)
 				if (resposta == 'Inserido com sucesso') {
 
-					alert(response);
 					$("#returnCadastroUsr").text("Cadastrado com sucesso");
 					$("#returnCadastroUsr").addClass("text-success");
 
+					setInterval(function(){
+						$("#returnCadastroUsr").css("display","none");
+						$("#txtNomeUsr").val("");
+						$("#txtEmail").val("");
+						$("#txtLogin").val("");
+						$("#txtSenhaUsr").val("");
+						$("#permissao").val("");
+						$("#statusUsuario").val("");
+					},5000);
 
 				}else
 				{
+					$("#returnCadastroUsr").text("Não Foi possivel cadastrar");
+					$("#returnCadastroUsr").addClass("text-danger");
 
+					setInterval(function(){
+						$("#returnCadastroUsr").css("display","none");
+						$("#txtNomeUsr").val("");
+						$("#txtEmail").val("");
+						$("#txtLogin").val("");
+						$("#txtSenhaUsr").val("");
+						$("#permissao").val("");
+						$("#statusUsuario").val("");
+					},5000);
 				}
 			}
 		});
