fa79d9a6244031a977757256f021a4f8f88c9ba7
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2018-06-25 20:07:36 -0300

    Endereco

--- notName/dal/EnderecoDAL.php
+++ notName/dal/EnderecoDAL.php
@@ -2,7 +2,7 @@
 require_once "../model/Endereco.php";
 require_once "../library/Conexao.class.php";
 
-class EnderecoDAL
+class EnderecoDAL extends Cliente
 {
 
     /**
@@ -33,10 +33,11 @@ class EnderecoDAL
         $uf = $end->getUF();
         
         // recebe a string sql
-        $sql = "";
+        $sql = "INSERT INTO ENDERECO (END_nCEP, END_cLOGRADOURO, END_cCIDADE, END_cBAIRRO,END_nNUMERO,END_cCOMPLEMENTO,END_cTIPO,UF_nID)
+                            VALUES ('$cep','$logradouro','$cidade','$bairro','$numero','$complemento','$tipo','$uf')";
         
         // Executa a string sql
-        EnderecoDAL::$connection->executarSQL($sql);
+        EnderecoDAL::$connection->sqlNoTransact($sql);
         
         return EnderecoDAL::$connection->returnID();
     }
@@ -44,9 +45,12 @@ class EnderecoDAL
     public static function buscaEndereco(Endereco $end): array
     {
         EnderecoDAL::connect();
-        
+        $cliente = new Cliente;
+
+        $id = $cliente->getIdCli;
+
         // String sql
-        $sql = "";
+        $sql = "SELECT * FROM ENDERECO E INNER JOIN CLIENTE_ENDERECO CE ON CE.END_nID = E.END_nID WHERE CLI_nCOD = $id";
         
         // Executa a string sql e atribui a variavel
         EnderecoDAL::$connection->executarSQL($sql);
@@ -82,10 +86,20 @@ class EnderecoDAL
     public static function atualizaEndereco(Endereco $end): string
     {
         EnderecoDAL::connect();
-        
+
         $idEnd = $end->getId();
+        $cep = $end->getCep();
+        $logradouro = $end->getLogradouro();
+        $cidade = $end->getCidade();
+        $bairro = $end->getBairro();
+        $numero = $end->getNumero();
+        $complemento = $end->getComplemento();
+        $tipoEnd = $end->getTipo();
+        $idUf = $end->getId();
         
-        $sql = "";
+        $sql = "UPDATE ENDERECO SET END_nCEP = '$cep', END_cLOGRADOURO = '$logradouro', END_cCIDADE = '$cidade', 
+                                    END_cBAIRRO = '$bairro', END_nNUMERO = '$numero', END_cCOMPLEMENTO = '$complemento', 
+                                    END_cTIPO = '$tipoEnd', UF_nID = '$idUf' WHERE END_nID = $idEnd ";
         
         return EnderecoDAL::$connection->executarSQL($sql);
     }
@@ -93,11 +107,8 @@ class EnderecoDAL
     public static function buscaUF(Endereco $end): string
     {
         EnderecoDAL::connect();
-        
-        $idUF = $end->getIdUF();
-        $UF = $end->getUF();
-        
-        $sql = "";
+              
+        $sql = "SELECT * FROM UF";
         
         EnderecoDAL::$connection->executarSQL($sql);
         
